7063ea9dece1de1c6982c989dc30b68446f74420
Ayush <ayush-devhub@gmail.com>
2026-03-07 09:56:51 +0530

    feat(views): add product and category viewsets

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
-# Create your views here.
+from products.models import Product, Category
+from products.serializers import ProductSerializer, CategorySerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CategoryViewSet(ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
